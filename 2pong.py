5ceb3de74b765c6f05aac73ded81d88046c9d437
Bryan Mendoza-Trejo <62330408+bryanmendozatrejo@users.noreply.github.com>
2021-03-04 15:49:14 -0800

    Add files via upload
    fixed some bugs.

--- 2pong.py
+++ 2pong.py
@@ -13,7 +13,7 @@ wn.bgpic("game_background.gif")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Paddle A
+#Creates Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.shape("square")
 paddle_a.color("white")
@@ -21,13 +21,22 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-# Paddle B
+#Creates Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.shape("square")
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
+
+# Creates Ball
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
 # Functions
 def paddle_a_up():
@@ -56,33 +65,31 @@ wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
 while True:
-    wn.update()
-    
-    # Move the ball
-	    ball.setx(ball.xcor() + ball.dx)
-	    ball.sety(ball.ycor() + ball.dy)
+	wn.update()
 
-	    # Border checking
-	    if ball.ycor() > 290:
-	        ball.sety(290)
-	        ball.dy *= -1
+	# Move the ball
+	ball.setx(ball.xcor() + ball.dx)
+	ball.sety(ball.ycor() + ball.dy)
 
-	    if ball.ycor() < -290:
-	        ball.sety(-290)
-	        ball.dy *= -1
+	# Border checking
+	if ball.ycor() > 290:
+		ball.sety(290)
+		ball.dy *= -1
+	if ball.ycor() < -290:
+		ball.sety(-290)
+		ball.dy *= -1
+	if ball.xcor() > 390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+		score_a += 1
+		pen.clear()
+		pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
+			font=("Courier", 24, "normal"))
 
-	    if ball.xcor() > 390:
-	        ball.goto(0, 0)
-	        ball.dx *= -1
-	        score_a += 1
-	        pen.clear()
-	        pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
-	                  font=("Courier", 24, "normal"))
-
-	    if ball.xcor() < -390:
-	        ball.goto(0, 0)
-	        ball.dx *= -1
-	        score_b += 1
-	        pen.clear()
-	        pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
-	                  font=("Courier", 24, "normal"))
+	if ball.xcor() < -390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+		score_b += 1
+		pen.clear()
+		pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
+			font=("Courier", 24, "normal"))
