ea708470cc7188f06b9c72299c32c8acf0ed4252
Mahdi Sabbouri <48333875+msabbouri@users.noreply.github.com>
2021-03-11 15:22:32 -0800

    Update 2pong.py

--- 2pong.py
+++ 2pong.py
@@ -6,37 +6,48 @@ pong.py
 
 import turtle
 
-wn = turtle.Screen()
-wn.title("Pong")
-wn.bgcolor("black")
-wn.bgpic("game_background.gif")
-wn.setup(width=800, height=600)
-wn.tracer(0)
+def main():
+	
+	# Score
+	score_a = 0
+	score_b = 0
 
-#Creates Paddle A
-paddle_a = turtle.Turtle()
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(-350, 0)
+	# Paddle A
+	paddle_a = turtle.Turtle()
+	paddle_a.speed(0)
+	paddle_a.shape("square")
+	paddle_a.color("white")
+	paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+	paddle_a.penup()
+	paddle_a.goto(-350, 0)
 
-#Creates Paddle B
-paddle_b = turtle.Turtle()
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
-paddle_b.goto(350, 0)
+	# Paddle B
+	paddle_b = turtle.Turtle()
+	paddle_b.speed(0)
+	paddle_b.shape("square")
+	paddle_b.color("white")
+	paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+	paddle_b.penup()
+	paddle_b.goto(350, 0)
 
-# Creates Ball
-ball = turtle.Turtle()
-ball.shape("circle")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
+	# Ball
+	ball = turtle.Turtle()
+	ball.shape("circle")
+	ball.color("white")
+	ball.penup()
+	ball.goto(0, 0)
+	ball.dx = 2
+	ball.dy = 2
+
+	# Pen
+	pen = turtle.Turtle()
+	pen.speed(0)
+	pen.color("white")
+	pen.penup()
+	pen.hideturtle()
+	pen.goto(0, 260)
+	pen.write("Player A: 0     Player B: 0", align="center",
+	          font=("Courier", 24, "normal"))
 
 # Functions
 def paddle_a_up():
