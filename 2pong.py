39aa7a369c715aba34a022559e9a2d9d791eee7e
Bryan Mendoza-Trejo <62330408+bryanmendozatrejo@users.noreply.github.com>
2021-03-11 16:58:18 -0800

    Add files via upload

--- 2pong.py
+++ 2pong.py
@@ -6,8 +6,35 @@ pong.py
 
 import turtle
 
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 def main():
 	
+	global wn, paddle_a, paddle_b, ball, pen
+
+	wn = turtle.Screen()
+	wn.title("Pong")
+	wn.bgcolor("black")
+	wn.bgpic("game_background.gif")
+	wn.setup(width=800, height=600)
+	wn.tracer(0)
+
 	# Score
 	score_a = 0
 	score_b = 0
@@ -49,58 +76,41 @@ def main():
 	pen.write("Player A: 0     Player B: 0", align="center",
 	          font=("Courier", 24, "normal"))
 
-# Functions
-def paddle_a_up():
-    y = paddle_a.ycor()
-    y += 20
-    paddle_a.sety(y)
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
-def paddle_b_up():
-    y = paddle_b.ycor()
-    y += 20
-    paddle_b.sety(y)
-def paddle_b_down():
-    y = paddle_b.ycor()
-    y -= 20
-    paddle_b.sety(y)
+	# Keyboard binding
+	wn.listen()
+	wn.onkeypress(paddle_a_up, "w")
+	wn.onkeypress(paddle_a_down, "s")
+	wn.onkeypress(paddle_b_up, "Up")
+	wn.onkeypress(paddle_b_down, "Down")
 
+	while True:
+		wn.update()
 
-# Keyboard binding
-wn.listen()
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
+		# Move the ball
+		ball.setx(ball.xcor() + ball.dx)
+		ball.sety(ball.ycor() + ball.dy)
 
-while True:
-	wn.update()
+		# Border checking
+		if ball.ycor() > 290:
+			ball.sety(290)
+			ball.dy *= -1
+		if ball.ycor() < -290:
+			ball.sety(-290)
+			ball.dy *= -1
+		if ball.xcor() > 390:
+			ball.goto(0, 0)
+			ball.dx *= -1
+			score_a += 1
+			pen.clear()
+			pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
+				font=("Courier", 24, "normal"))
 
-	# Move the ball
-	ball.setx(ball.xcor() + ball.dx)
-	ball.sety(ball.ycor() + ball.dy)
+		if ball.xcor() < -390:
+			ball.goto(0, 0)
+			ball.dx *= -1
+			score_b += 1
+			pen.clear()
+			pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
+				font=("Courier", 24, "normal"))
 
-	# Border checking
-	if ball.ycor() > 290:
-		ball.sety(290)
-		ball.dy *= -1
-	if ball.ycor() < -290:
-		ball.sety(-290)
-		ball.dy *= -1
-	if ball.xcor() > 390:
-		ball.goto(0, 0)
-		ball.dx *= -1
-		score_a += 1
-		pen.clear()
-		pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
-			font=("Courier", 24, "normal"))
-
-	if ball.xcor() < -390:
-		ball.goto(0, 0)
-		ball.dx *= -1
-		score_b += 1
-		pen.clear()
-		pen.write("Player A: {}     Player B: {}".format(score_a, score_b), align="center",
-			font=("Courier", 24, "normal"))
+main()
